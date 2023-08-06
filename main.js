e49f484b670de9a81e23f26bc72480ccd850665b
laksamanaap <laksamana.arya1412@gmail.com>
2023-08-06 22:04:10 +0700

    feat : set time function

--- main.js
+++ main.js
@@ -8,13 +8,9 @@ const toggle = document.querySelector('.toggle')
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-
-function setTime() { };
-
 // Dark light mode
 toggle.addEventListener("click", (e) => {
     const html = document.querySelector("html");
-
     if (html.classList.contains('dark')) {
         html.classList.remove('dark');
         e.target.innerHTML = "Light Mode";
@@ -22,7 +18,33 @@ toggle.addEventListener("click", (e) => {
         html.classList.add('dark');
         e.target.innerHTML = "Dark Mode";
     }
-
 })
 
+// Set time
+function setTime() {
+    const time = new Date();
+    const month = time.getMonth()
+    const day = time.getDay()
+    const date = time.getDate()
+    const hours = time.getHours()
+    const Oclock = hours >= 13 ? hours % 12 : hours;
+    const minutes = time.getMinutes()
+    const seconds = time.getSeconds()
+    const ampm = hours >= 12 ? 'PM' : 'AM'
 
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(Oclock, 0, 12, 0, 360)}deg)`
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 60, 0, 360)}deg)`
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 60, 0, 360)}deg)`
+
+    timeEl.innerHTML = `${Oclock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+    dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
+}
+
+// Move arrow direction
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+setTime();
+
+setInterval(setTime, 1500)
